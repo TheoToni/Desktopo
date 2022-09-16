19aab6f13da0fef8d42bcca4b7597a1702012c23
TheoToni <32997269+TheoToni@users.noreply.github.com>
2022-09-16 16:54:20 +0200

    Glitch Effect

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,33 @@ import taskbar from "./assets/taskbar.png";
 import folder from "./assets/folder.png";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [screens, setScreens] = useState(0);
 
   return (
     <div className="App">
-      <img className="taskbar" src={taskbar} alt="" />
-      <div className="documents">
-        Documents
-        <img src={folder} alt="" />
+      {screens === 0 && (
+        <div className="firstScreen">
+          <img className="taskbar" src={taskbar} alt="" />
+          <div className="documents">
+            Documents
+            <img src={folder} alt="" />
+          </div>
+          <div className="games">
+            Games
+            <img src={folder} alt="" />
+          </div>
+          <div className="images">
+            Images
+            <img src={folder} alt="" />
+          </div>
+        </div>
+      )}
+      <div className="secondScreen">
+        <h1 class="glitch">
+          <span aria-hidden="true">404 Error</span>
+          404 Error
+          <span aria-hidden="true">404 Error</span>
+        </h1>
       </div>
     </div>
   );
